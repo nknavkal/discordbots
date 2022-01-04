bcad4138d368a3b4530c87f2abcb86603f4feb21
Nishaad Navkal <nknavkal@gmail.com>
2022-01-03 22:07:55 -0600

    tags most recent pinger

--- firstbot/bot.js
+++ firstbot/bot.js
@@ -1,24 +1,24 @@
 // Run dotenv
 require('dotenv').config();
 const { Client, Intents } = require('discord.js');
+const BOT_ID = '927705596952727622';
+var pinger_ID;
 
 const client = new Client({
-	intents: [
-	Intents.FLAGS.GUILDS,
-	Intents.FLAGS.GUILD_MESSAGES
-	] 
+	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] 
 });
-
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('messageCreate', (msg) => {
-	console.log("AYO");
   	if (msg.content === 'ping') {
-    	msg.reply('pong');
-  	};
+  		msg.reply('pong');
+  		pinger_ID = msg.author.id;
+  	} else if (msg.content === 'who pinged?') {
+  		msg.reply('<@' + pinger_ID + '> did.');
+  	}
 });
 
 client.login(process.env.DISCORD_TOKEN);
